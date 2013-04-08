b7e5b35d918c041aa02b84560a1207516fa78b81
Mike Thorn <mthorn@bahamut.(none)>
2013-04-08 10:56:41 -0400

    adding basic code structure

---
+++ lib/php/jvc.php
@@ -0,0 +1,26 @@
+<?php
+# Copyright 2013 Mike Thorn (github: WasabiVengeance). All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+global $jvc;
+
+class jvc
+{
+	function init($base_dir,$config_file)
+	{
+		$jvc = array(
+			
+		);
+	}
+	
+	function process()
+	{
+	}
+	
+	function deinit()
+	{
+	}
+}
+
+?>
---
+++ lib/php/jvc_controller.php
@@ -0,0 +1,21 @@
+<?php
+# Copyright 2013 Mike Thorn (github: WasabiVengeance). All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+class jvc_controller
+{
+	function init()
+	{
+	}
+	
+	function deinit()
+	{
+	}
+	
+	function __call()
+	{
+	}
+}
+
+?>
